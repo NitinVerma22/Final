d79b530e8f831f4bb615b0a6cbaefb37c937ffc1
Nitin Kumar Verma <nitinkumarv2021@gmail.com>
2025-06-18 14:13:58 +0530

    Update page.tsx

--- app/[short]/page.tsx
+++ app/[short]/page.tsx
@@ -2,11 +2,7 @@
 import links from '../data/links.json'
 import { redirect, notFound } from 'next/navigation'
 
-type Props = {
-  params: { short: string }
-}
-
-export default function ShortRedirect({ params }: Props) {
+export default function ShortRedirect({ params }: any) {
   const url = (links as Record<string, string>)[params.short]
 
   if (url) {
